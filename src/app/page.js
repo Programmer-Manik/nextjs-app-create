a27c4789a9c63adf8eb7e04e1c41307b7cad50df
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-03-10 03:05:21 +0600

    Advance Data Fetching Strategies With SSR

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,24 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "home page",
-  description:'this is home page '
+  description: "this is home page ",
 };
 const Page = async () => {
-  const res = await fetch("http://localhost:5000/shoes",{
+  const res = await fetch("http://localhost:5000/shoes", {
     // cache:"force-cache"
-    next:{
-      revalidate:5,
+    next: {
+      revalidate: 5,
     },
   });
   const shoes = await res.json();
   // console.log(data)
   return (
-    <div>
+    <div className="text-center py-2 ">
       <h2 className="text-center text-4xl">Welcome to the Page page</h2>
       <div className=" flex justify-around items-center p-5">
-        {shoes.slice(0 , 3).map((data) => (
-          <div 
-          key={data.id}
-          className="card w-80 bg-base-100 shadow-xl">
+        {shoes.slice(0, 3).map((data) => (
+          <div key={data.id} className="card w-80 bg-base-100 shadow-xl">
             <figure>
               <img
                 src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
@@ -27,18 +27,21 @@ const Page = async () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">
-               {data.title}
+                {data.title}
                 <div className="badge badge-secondary">{data.price}</div>
               </h2>
               <p>{data.description}</p>
               <div className="card-actions justify-end">
-              <button className="btn btn-outline btn-primary">Bay Now</button>
-              <button className="btn btn-outline btn-primary">details</button>
+                <button className="btn btn-outline btn-primary">Bay Now</button>
+                <button className="btn btn-outline btn-primary">details</button>
               </div>
             </div>
           </div>
         ))}
       </div>
+      <Link href="/all-shoes">
+        <button className="btn btn-outline btn-primary">see more</button>
+      </Link>
     </div>
   );
 };

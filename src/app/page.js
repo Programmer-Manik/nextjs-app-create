5500a72ebea33cbb266dd126c9d333632ba2134f
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-03-10 00:33:26 +0600

    Set-Up Json Server For Next Level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,38 @@
 export const metadata = {
-  title:'home page'
-}
-const Page = () => {
+  title: "home page",
+};
+const Page = async () => {
+  const res = await fetch("http://localhost:5000/shoes");
+  const shoes = await res.json();
+  // console.log(data)
   return (
     <div>
       <h2 className="text-center text-4xl">Welcome to the Page page</h2>
+      <div className=" flex justify-around items-center p-5">
+        {shoes.slice(0 , 3).map((data) => (
+          <div 
+          key={data.id}
+          className="card w-80 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
